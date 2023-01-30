4c1b13081c6d91260d44a70d4ebdf1c806a81d4a
tiagorahal <rahal.aires@gmail.com>
2023-01-30 11:42:38 -0300

    Add correct positioning to header elements

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,10 +5,41 @@ type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <header>
-      <div></div>
+    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
+      <div className='flex flex-row items-center'>
 
-      <div></div>
+        <SocialIcon
+          url='https://www.youtube.com'
+          fgColor='gray'
+          bgColor='transparent'
+        />
+
+        <SocialIcon
+          url='https://www.youtube.com'
+          fgColor='gray'
+          bgColor='transparent'
+        />
+
+        <SocialIcon
+          url='https://www.youtube.com'
+          fgColor='gray'
+          bgColor='transparent'
+        />
+
+      </div>
+
+      <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+
+        <SocialIcon
+          className='cursor-pointer'
+          network='email'
+          fgColor='gray'
+          bgColor='transparent'
+        />
+
+        <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get In Touch</p>
+
+      </div>
     </header>
   )
 }
