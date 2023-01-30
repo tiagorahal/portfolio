dfc9f27ee8927cbd3f63981cac0d2cf145bcf56a
tiagorahal <rahal.aires@gmail.com>
2023-01-30 19:00:09 -0300

    Finish experienceCard component, Add experience section

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -1,13 +1,27 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import ExperienceCard from './experienceCard'
 
 type Props = {}
 
 export default function Experience({}: Props) {
   return (
-    <div className='h-screen flex relative overflow-hidden flex-col text-left'>
-      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+    <motion.div
+      className='h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-5 justify-evenly mx-auto items-center'
+      initial={{ opacity: 0}}
+      whileInView={{ opacity: 1}}
+      transition={{ duration: 1.5 }}
+    >
+      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl my-5'>
         Experience
       </h3>
-    </div>
+
+      <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory h-4/6'>
+        <ExperienceCard />
+        <ExperienceCard />
+        <ExperienceCard />
+        <ExperienceCard />
+      </div>
+    </motion.div>
   )
 }
