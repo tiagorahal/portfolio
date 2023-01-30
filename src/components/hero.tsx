683ba778da2a69a0294dafcf2b65f0a6a6819683
tiagorahal <rahal.aires@gmail.com>
2023-01-30 15:29:47 -0300

    Finish BackgroundCircles component

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import BackgroundCircles from './backgroundCircles'
+import { Cursor, useTypewriter } from 'react-simple-typewriter'
 
 type Props = {}
 
 export default function Hero({}: Props) {
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi, The name's Tiago Rahal",
+      "Dude-who-loves-videogames.tsx",
+      "<ButLovesToCodeMore />"
+    ],
+    loop: true,
+    delaySpeed: 2000
+  })
   return (
-    <div>Hellooooooooooo</div>
+    <div>
+      <BackgroundCircles />
+      <h1>
+        <span>{text}</span>
+        <Cursor cursorColor='#f7ab0a' />
+      </h1>
+    </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen'>
       <Head>
         <title>Tiago Rahal</title>
         <meta name="description" content="Tiago Rahal Aires Portfolio" />
