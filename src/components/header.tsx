6b29beaecf5a5efb0d7b0edfddbd1bc4de607ee4
tiagorahal <rahal.aires@gmail.com>
2023-01-30 14:06:25 -0300

    Finish header structure, add animations

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,45 +1,68 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons'
+import { motion } from 'framer-motion'
 
 type Props = {}
 
 export default function Header({}: Props) {
   return (
     <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
-      <div className='flex flex-row items-center'>
-
+      <motion.div
+        className='flex flex-row items-center'
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1
+        }}
+        transition={{
+          duration: 1.5
+        }}
+      >
         <SocialIcon
           url='https://www.youtube.com'
           fgColor='gray'
           bgColor='transparent'
         />
-
         <SocialIcon
           url='https://www.youtube.com'
           fgColor='gray'
           bgColor='transparent'
         />
-
         <SocialIcon
           url='https://www.youtube.com'
           fgColor='gray'
           bgColor='transparent'
         />
-
-      </div>
-
-      <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
-
+      </motion.div>
+      <motion.div
+        className='flex flex-row items-center text-gray-300 cursor-pointer'
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1
+        }}
+        transition={{
+          duration: 1.5
+        }}
+      >
         <SocialIcon
           className='cursor-pointer'
           network='email'
           fgColor='gray'
           bgColor='transparent'
         />
-
         <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get In Touch</p>
-
-      </div>
+      </motion.div>
     </header>
   )
 }
