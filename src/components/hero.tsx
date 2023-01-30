55a35a3eb51fe4e00fbeb8a8492edcc7f4b377ea
tiagorahal <rahal.aires@gmail.com>
2023-01-30 16:37:24 -0300

    Finish Hero section

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import BackgroundCircles from './backgroundCircles'
+import Link from 'next/link'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 
 type Props = {}
@@ -15,12 +16,35 @@ export default function Hero({}: Props) {
     delaySpeed: 2000
   })
   return (
-    <div>
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
       <BackgroundCircles />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor='#f7ab0a' />
-      </h1>
+      <img
+        className='relative rounded-full h-32 w-32'
+        src='https://avatars.githubusercontent.com/u/77216006?v=4'
+        alt='Profile Pic'
+      />
+      <div className='z-20'>
+        <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Software Engineer</h2>
+        <h1 className='text-5xl lg:text-6xl font-semibold scroll-px-10'>
+          <span className='mr-3'>{text}</span>
+          <Cursor cursorColor='#f7ab0a' />
+        </h1>
+
+        <div className='pt-5'>
+          <Link href="#about">
+            <button className='heroButton'>About</button>
+          </Link>
+          <Link href="#experience">
+            <button className='heroButton'>Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className='heroButton'>Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className='heroButton'>Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen'>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title>Tiago Rahal</title>
         <meta name="description" content="Tiago Rahal Aires Portfolio" />
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <section id='hero'>
+      <section id='hero' className='snap-center'>
         <Hero />
       </section>
       
