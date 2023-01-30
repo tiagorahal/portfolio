075b72e4c0d18c6cb013fed55134e972f8d9755e
tiagorahal <rahal.aires@gmail.com>
2023-01-30 14:20:16 -0300

    Add React Simple Typewriter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Header from '@/components/header'
+import Hero from '@/components/hero'
 import { Inter } from '@next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -13,8 +14,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
       <Header />
+      <section id='hero'>
+        <Hero />
+      </section>
       
     </div>
   )
