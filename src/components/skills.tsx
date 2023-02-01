8d71bd9bfaa7304766e809346ab393493505f4f1
tiagorahal <rahal.aires@gmail.com>
2023-02-01 16:29:49 -0300

    Add skills

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 
+import Javascript from  '../../public/js.png'
+import ReactJs from '../../public/react.png'
+import RoR from '../../public/ror.png'
+import AWS from '../../public/aws.png'
+import Docker from '../../public/docker.png'
+import Java from '../../public/java.png'
+import MySql from '../../public/mysql.png'
+import PHP from '../../public/php.png'
+import Postgres from '../../public/postgres.png'
+import Ruby from '../../public/ruby.png'
+
 type Props = {
   directionLeft?: boolean;
 }
@@ -22,7 +33,7 @@ export default function Skills({ directionLeft }: Props) {
       <div className='grid grid-cols-4 gap-5'>
 
 
-        
+      {/* RoR */}
       <div className='group relative flex cursor-pointer'>
         <motion.img
           initial={{
@@ -31,14 +42,212 @@ export default function Skills({ directionLeft }: Props) {
           }}
           transition={{ duration: 1 }}
           whileInView={{ opacity: 1, x: 0}}
-          src="https://seeklogo.com/images/V/vans-of-the-wall-logo-BDF59B4967-seeklogo.com.png"
+          src={RoR.src}
           className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32
           xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
         />
         <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
           <div className='flex items-center justify-center h-full'>
             <p className='text-3xl font-bold text-black opacity-100'>
-              100%
+              90%
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Ruby */}
+      <div className='group relative flex cursor-pointer'>
+        <motion.img
+          initial={{
+            x: directionLeft ? -200 : 200,
+            opacity: 0
+          }}
+          transition={{ duration: 1 }}
+          whileInView={{ opacity: 1, x: 0}}
+          src={Ruby.src}
+          className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32
+          xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
+        />
+        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
+          <div className='flex items-center justify-center h-full'>
+            <p className='text-3xl font-bold text-black opacity-100'>
+              90%
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* JS */}
+      <div className='group relative flex cursor-pointer'>
+        <motion.img
+          initial={{
+            x: directionLeft ? -200 : 200,
+            opacity: 0
+          }}
+          transition={{ duration: 1 }}
+          whileInView={{ opacity: 1, x: 0}}
+          src={Javascript.src}
+          className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32
+          xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
+        />
+        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
+          <div className='flex items-center justify-center h-full'>
+            <p className='text-3xl font-bold text-black opacity-100'>
+              90%
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* React */}
+      <div className='group relative flex cursor-pointer'>
+        <motion.img
+          initial={{
+            x: directionLeft ? -200 : 200,
+            opacity: 0
+          }}
+          transition={{ duration: 1 }}
+          whileInView={{ opacity: 1, x: 0}}
+          src={ReactJs.src}
+          className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32
+          xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
+        />
+        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
+          <div className='flex items-center justify-center h-full'>
+            <p className='text-3xl font-bold text-black opacity-100'>
+              70%
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* AWS */}
+      <div className='group relative flex cursor-pointer'>
+        <motion.img
+          initial={{
+            x: directionLeft ? -200 : 200,
+            opacity: 0
+          }}
+          transition={{ duration: 1 }}
+          whileInView={{ opacity: 1, x: 0}}
+          src={AWS.src}
+          className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32
+          xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
+        />
+        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
+          <div className='flex items-center justify-center h-full'>
+            <p className='text-3xl font-bold text-black opacity-100'>
+              60%
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Java */}
+      <div className='group relative flex cursor-pointer'>
+        <motion.img
+          initial={{
+            x: directionLeft ? -200 : 200,
+            opacity: 0
+          }}
+          transition={{ duration: 1 }}
+          whileInView={{ opacity: 1, x: 0}}
+          src={Java.src}
+          className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32
+          xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
+        />
+        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
+          <div className='flex items-center justify-center h-full'>
+            <p className='text-3xl font-bold text-black opacity-100'>
+              40%
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* PHP */}
+      <div className='group relative flex cursor-pointer'>
+        <motion.img
+          initial={{
+            x: directionLeft ? -200 : 200,
+            opacity: 0
+          }}
+          transition={{ duration: 1 }}
+          whileInView={{ opacity: 1, x: 0}}
+          src={PHP.src}
+          className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32
+          xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
+        />
+        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
+          <div className='flex items-center justify-center h-full'>
+            <p className='text-3xl font-bold text-black opacity-100'>
+              40%
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Docker */}
+      <div className='group relative flex cursor-pointer'>
+        <motion.img
+          initial={{
+            x: directionLeft ? -200 : 200,
+            opacity: 0
+          }}
+          transition={{ duration: 1 }}
+          whileInView={{ opacity: 1, x: 0}}
+          src={Docker.src}
+          className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32
+          xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
+        />
+        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
+          <div className='flex items-center justify-center h-full'>
+            <p className='text-3xl font-bold text-black opacity-100'>
+              30%
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* postgres */}
+      <div className='group relative flex cursor-pointer'>
+        <motion.img
+          initial={{
+            x: directionLeft ? -200 : 200,
+            opacity: 0
+          }}
+          transition={{ duration: 1 }}
+          whileInView={{ opacity: 1, x: 0}}
+          src={Postgres.src}
+          className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32
+          xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
+        />
+        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
+          <div className='flex items-center justify-center h-full'>
+            <p className='text-3xl font-bold text-black opacity-100'>
+              70%
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* mysql */}
+      <div className='group relative flex cursor-pointer'>
+        <motion.img
+          initial={{
+            x: directionLeft ? -200 : 200,
+            opacity: 0
+          }}
+          transition={{ duration: 1 }}
+          whileInView={{ opacity: 1, x: 0}}
+          src={MySql.src}
+          className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32
+          xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
+        />
+        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
+          <div className='flex items-center justify-center h-full'>
+            <p className='text-3xl font-bold text-black opacity-100'>
+              70%
             </p>
           </div>
         </div>
