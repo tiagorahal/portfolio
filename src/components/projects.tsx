063a01696a117f12242310d5429bc38d19b07dc3
tiagorahal <rahal.aires@gmail.com>
2023-02-01 11:45:50 -0300

    Customize scrollbars

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -17,7 +17,7 @@ export default function Projects({}: Props) {
         Projects
       </h3>
 
-      <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
+      <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
         {
           projects.map((project, i) =>(
             <motion.div
