36e25fcb3a1f11e6ed73f41ac4f0815f954fdb62
tiagorahal <rahal.aires@gmail.com>
2023-02-02 17:46:15 -0300

    Fix typo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
       <Head>
-        <title>Tiago Rahal</title>
+        <title>Tiago's Portfolio</title>
         <meta name="description" content="Tiago Rahal Aires Portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
